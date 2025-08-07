08096a3dfb307f7be04ca8ecfecf48defb8d40b4
Gabe-Fernandes <gabrielcatanifernandes@gmail.com>
2025-08-07 16:04:32 -0400

    8/7/25 Wedding countdown feature implemented

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log("hello world");
+// Countdown Feature
+const currentDate = new Date();
+const weddingDate = new Date("2026-08-16");
+const daysToGo = Math.round((weddingDate.getTime() - currentDate.getTime()) / (1000 * 60 * 60 * 24));
+$("#countDown").html(`${daysToGo} DAYS TO GO!`);
