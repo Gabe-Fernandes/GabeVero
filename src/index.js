6a5d60b89451797f763e34cd37208112377a493b
Gabe-Fernandes <gabrielcatanifernandes@gmail.com>
2025-08-09 14:37:50 -0400

    8/9/25 navbar underline styling added

--- src/index.js
+++ src/index.js
@@ -3,3 +3,13 @@ const currentDate = new Date();
 const weddingDate = new Date("2026-08-16");
 const daysToGo = Math.round((weddingDate.getTime() - currentDate.getTime()) / (1000 * 60 * 60 * 24));
 $("#countDown").html(`${daysToGo} DAYS TO GO!`);
+
+// Navbar Underline
+$(".nav-wrap .btn").hover(
+    function(event) {
+        $(event.target).siblings(".nav-underline").css("opacity", "100%");
+    },
+    function(event) {
+        $(event.target).siblings(".nav-underline").css("opacity", "0%");
+    }
+);
